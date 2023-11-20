00d5d07ede4d04076903f981735b0d01ab927999
Mazantii <90619558+Mazantii@users.noreply.github.com>
2023-11-20 15:46:42 +0100

    Formen virker! og der er regexet så man kun kan skrive tal i de relevante felter!

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ def PostData(name, buy_price, sold_price, my_evaluation, amount, hjemme, pokemon
     "amount": amount, "hjemme": hjemme, "pokemon_id": pokemon_id, "holo": holo, "comment": comment}
     collection.insert_one(post)
 
-PostData("seiuhiuhe", 100, 200, "Near Mint", 1, True, "XY12", "Holo", "")
+#PostData("seiuhiuhe", 100, 200, "Near Mint", 1, Hjemme, "XY12", "Holo", "")
 #For all of the data in the collection find the ones with amount 1 or above and make them into a array
 available = {}
 not_home = {}
@@ -27,14 +27,12 @@ def GiveAvailable():
 
 #This is for the card that are out being sold
 def NotHome():
-    #Make a new array containing all of the cards that are not at home and have a amount of 1 or more
+    #Make a new array containing all of the cards that are "Ude" and have a amount of 1 or more
     for x in collection.find():
-        if x["amount"] >= 1 and x["hjemme"] == False:
-            #if there is a match make a new object in the available array with the name as the key and the sold_price, my_evaluation and amount as the value
+        if x["hjemme"] == "Ude" and x["amount"] >= 1:
             not_home[x["name"]] = [x["sold_price"], x["my_evaluation"], x["amount"], x["hjemme"], x["buy_price"], x["pokemon_id"],
              x["holo"], x["comment"]]
 
-GiveAvailable()
 #access the name of the first element in the array
 print(available)
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import main
 import db
 
@@ -10,8 +10,6 @@ main.UpdateSales()
 earnings = main.earnings
 amount = main.total_amount_of_sales
 
-#Run the GiveAvailable function
-db.GiveAvailable()
 
 #values from db.py
 available = db.available
@@ -24,13 +22,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    db.NotHome()
     #Send the earnings to the index.html file
     return render_template("index.html", earnings=earnings, sales=amount, not_available=not_available)
 
 @app.route("/database")
 def database():
-    #Send the earnings to the index.html file
+    db.GiveAvailable()
+    #Send the available array to the database.html file
     return render_template("database.html", available=available)
+
+@app.route("/addcard", methods=["POST"])
+def addcard():
+    #Get the data from the form
+    name = request.form["name"]
+    buy_price = int(request.form["buy_price"])
+    sold_price = int(request.form["sell_price"])
+    evaluation = request.form["evaluation"]
+    amount = int(request.form["amount"])
+    hjemme = request.form["out_home"]
+    pokemon_id = request.form["version"]
+    holo = request.form["rev_hol"]
+    comment = request.form["comment"]
+
+    print(name, buy_price, sold_price, evaluation, amount, hjemme, pokemon_id, holo, comment)
+    #Post it to the db!
+    db.PostData(name, buy_price, sold_price, evaluation, amount, hjemme, pokemon_id, holo, comment)
+    #Update the available array
+    db.GiveAvailable()
+    return render_template("database.html" , available=available)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
